1d547b20f6bdb3242a32d566f42ba540eac16dab
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2020-11-28 11:04:22 +0600

    Removed It cuz this was bullshit

--- binary-denery.py
+++
@@ -1,9 +0,0 @@
-binery = list(input("Input a binary number: "))
-value = 0
-
-for i in range(len(binery)):
-	digit = binery.pop()
-	if digit == '1':
-		value = value + pow(2, i)
-print("the denery value is : ", value)
-
