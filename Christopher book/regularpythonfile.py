02f92009f15c9c551acce76a41d37634c8574a0f
Ariq100 <shadmanmuhtasim2005@gmail.com>
2024-08-03 23:12:50 +0600

    A proper understanding of queue's and stack's

---
+++ Christopher book/regularpythonfile.py
@@ -0,0 +1,40 @@
+#Stacks
+
+# i = int(input("Enter the amount of entries: "))
+# val = []
+
+# for x in range(0, i):
+#     UserInput = str(input("Enter: "))
+    
+#     val.append(UserInput)
+    
+# for x in range(0, i):
+#     print(val.pop())
+    
+# print(len(val))
+
+
+
+
+
+#Queue
+# Do it with Classes and Functions 
+
+# lineIn = []
+# lineOut = []
+
+# i = int(input("Enter the number of tries: "))
+
+# for x in range(0, i):
+#     UserInput = str(input("Enter: "))
+    
+#     lineIn.append(UserInput)
+
+
+# length = len(lineIn)
+
+# for i in range(0, length):
+#     lineOut.append(lineIn[length - i - 1])
+
+# for i in range(0, len(lineOut)):
+#     print(lineOut.pop())
